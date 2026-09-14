74608458bae6e036b02711d71b518582805afad8
kabacklund <x.mankevin@hotmail.com>
2026-09-14 13:30:54 +0200

    Basics

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
+using Azure;
+using Azure.AI.TextAnalytics;
+using DotNetEnv;
+
+Env.Load();
+string endpoint = Environment.GetEnvironmentVariable("ENDPOINT") ?? throw new Exception("fel endpoint");
+string key = Environment.GetEnvironmentVariable("KEY") ?? throw new Exception("fel key");
+
+var client = new TextAnalyticsClient(
+    new Uri(endpoint),
+    new AzureKeyCredential(key)
+);
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
